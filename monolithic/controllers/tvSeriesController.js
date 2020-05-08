6563384b799d20cfb70e193f1876b4b85a70238e
2maleek <222malikibrahim@gmail.com>
2020-05-08 11:23:18 +0700

    monolithic: CRUD /movies and /tvseries

---
+++ monolithic/controllers/tvSeriesController.js
@@ -0,0 +1,78 @@
+const { TvSerie } = require('../models')
+
+class TvSeriesController {
+  static createTvSerie(req, res, next) {
+    TvSerie.createTvSerie(req.body)
+      .then(result => {
+        res.status(201).json(result.ops)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+
+  }
+
+  static findAllTvSeries(req, res, next) {
+    TvSerie.findAllTvSeries()
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static findTvSerieById(req, res, next) {
+    TvSerie.findTvSerieById(req.params.id)
+      .then(result => {
+        if(!result) {
+          throw res.status(404).json({message: 'Tv Serie not found'})
+        }
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static updateTvSerieById(req, res, next) {
+    let {id} = req.params
+    TvSerie.findTvSerieById(id)
+      .then(isFound => {
+        if(isFound) {
+          return TvSerie.updateTvSerieById(id, req.body)
+        }
+        throw res.status(404).json({message: 'Tv Serie not found'})
+      })
+      .then(() => {
+        return TvSerie.findTvSerieById(req.params.id)
+      })
+      .then(updatedTvSerie => {
+        res.status(200).json(updatedTvSerie)
+      })
+
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static deleteTvSerieById(req, res, next) {
+    let {id} = req.params
+    TvSerie.findTvSerieById(id)
+      .then(isFound => {
+        if(isFound) {
+          return TvSerie.deleteTvSerieById(req.params.id)
+        }
+        throw res.status(404).json({message: 'Tv Serie not found'})
+      })
+      .then(result => {
+        res.status(200).json({message: 'delete successfully'})
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+}
+
+module.exports = TvSeriesController
---
+++ monolithic/models/index.js
@@ -0,0 +1,4 @@
+const Movie = require('./movie')
+const TvSerie = require('./tvSerie')
+
+module.exports = { Movie, TvSerie }
