d190a1255845e0d4fe050fc434fb772d895aacec
2maleek <222malikibrahim@gmail.com>
2020-05-10 10:23:36 +0700

    delete monolithoc

--- monolithic/controllers/index.js
+++
@@ -1,4 +0,0 @@
-const MoviesController = require('./moviesController')
-const TvSeriesController = require('./tvSeriesController')
-
-module.exports = { MoviesController, TvSeriesController }
--- monolithic/models/index.js
+++
@@ -1,4 +0,0 @@
-const Movie = require('./movie')
-const TvSerie = require('./tvSerie')
-
-module.exports = { Movie, TvSerie }
