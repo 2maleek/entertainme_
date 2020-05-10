181cc6030cde19318dda88487bd82b78ed2e9166
2maleek <222malikibrahim@gmail.com>
2020-05-10 11:52:08 +0700

    change db to entertainmr

--- tvSeriesServices/config/config.js
+++ tvSeriesServices/config/config.js
@@ -1,24 +1,27 @@
-const { MongoClient } = require('mongodb')
-const url = 'mongodb://localhost:27017'
-const dbName = "tv-series-services"
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const dbName = "entertainme";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-let db
+let db;
 
 function connect(callback) {
   client.connect(function(err) {
-    if(err) {
-      console.log('failed connect to mongodb')
-    }else {
-      console.log('successfully connect to mongodb')
-      db = client.db(dbName)
+    if (err) {
+      console.log("failed connect to mongodb");
+    } else {
+      console.log("successfully connect to mongodb");
+      db = client.db(dbName);
     }
-    callback(err)
-  })
+    callback(err);
+  });
 }
 
 function getDatabase() {
-  return db
+  return db;
 }
 
-module.exports = { connect, getDatabase }
+module.exports = { connect, getDatabase };
